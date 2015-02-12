d3e2955889fea6965f017ad9f0efe8e9675a604b
root <root@broker00.ose.opentlc.com>
2015-02-12 16:16:57 -0500

    ..

--- gpe_student_mgmt.sh
+++ gpe_student_mgmt.sh
@@ -13,10 +13,10 @@ main() {
         checkparams
         checkUserExists
         disenroll
-    elif [ "$op" = "batchdisenrollbygears" ]; then
+    elif [ "$op" = "batchDisenrollFromAllByGears" ]; then
         batchdisenrollbygears
-    elif [ "$op" = "batchdisenrollbyaccounts" ]; then
-        batchdisenrollbyaccounts
+    elif [ "$op" = "batchDisenrollFromAllByAccounts" ]; then
+        batchDisenrollFromAllByAccounts
     elif [ "$op" = "enroll" ]; then
         checkparams
         if [ ! -f /etc/openshift/broker.conf ]; then
@@ -41,81 +41,12 @@ check() {
     fi
 }
 
-batchdisenrollbygears() {
-    mongoUser=`cat /etc/openshift/broker.conf | egrep -i 'MONGO_USER='`
-    mongoUser=${mongoUser#*=}
-    mongoUser=${mongoUser//\"}
-
-    mongoPasswd=`cat /etc/openshift/broker.conf | egrep -i 'MONGO_PASSWORD='`
-    mongoPasswd=${mongoPasswd#*=}
-    mongoPasswd=${mongoPasswd//\"}
-
-    mongoDb=`cat /etc/openshift/broker.conf | egrep -i 'MONGO_DB='`
-    mongoDb=${mongoDb#*=}
-    mongoDb=${mongoDb//\"}
-    echo -en "\nbatchdisenrollbygears() mongoUser=$mongoUser mongoPasswd=$mongoPasswd mongoDb=$mongoDb \n "
-
-    for gearId in $(cat gears.txt)
-    do
-        if [[ $gearId != \#* ]] ; then
-            eval userString=\" `mongo --username $mongoUser --password $mongoPasswd $mongoDb --eval "printjson(db.applications.find( { _id: ObjectId(\"$gearId\") }, { members: 1 } ).shellPrint() )"` \"
-            userId=`echo $userString | cut -d':' -f 8 | cut -d' ' -f 2 | cut -d',' -f 1`
-            echo -en "\n\n*********     $gearId userId=$userId\n"
-            disenroll
-        fi
-    done
+destroyApp() {
+    echo -en "\ndestroyApp() userId = $1 appId = $2 \n"
+    /usr/sbin/oo-admin-ctl-app -l $userId -a $app -c stop
+    /usr/sbin/oo-admin-ctl-app -l $userId -a $app -c destroy -b
 }
 
-batchdisenrollbyaccounts() {
-    for userId in $(cat accounts.txt)
-    do
-        if [[ $userId != \#* ]] ; then
-            disenroll
-        fi
-    done
-}
-
-disenroll() {
-    echo -en "\ndisenroll(): disenrolling $userId from all courses\n"
-    arr=`/usr/sbin/oo-admin-ctl-domain -l $userId | egrep -i '^name:'`
-    if [ -n "$arr" ]; then
-        for app in $arr;
-        do
-         if [ "$app" != "name:" ]; then
-             /usr/sbin/oo-admin-ctl-app -l $userId -a $app -c stop
-             /usr/sbin/oo-admin-ctl-app -l $userId -a $app -c destroy -b
-         fi
-       done
-    fi
-    arr=`/usr/sbin/oo-admin-ctl-user -l $userId | egrep -i '  gear sizes:'|sed -e "s/.*:\s//"|sed -e "s/,//g"`
-    if [ -n "$arr" ]; then
-        for gearSize in $arr;
-        do
-           gearSize=${gearSize//,} 
-           gearSize=${gearSize//\ } 
-           if [ "$gearSize" != "pds_medium" ]; then
-            echo "Removing gear size $gearSize for user $userId"
-            /usr/sbin/oo-admin-ctl-user -l $userId --removegearsize $gearSize >/dev/null 2>&1
-           fi
-        done
-    fi
-}
-
-enroll() {
-    id $userId > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo "Error, user $userId does not exist in SSO.  Exiting."
-        exit 1
-    fi
-    check
-    echo -en "\nenroll():  enrolling $userId to course with gearSize = $gearSize\n"
-    /usr/sbin/oo-admin-ctl-user -c -l $userId --addgearsize $gearSize > /dev/null 2>&1
-    if [ $? -ne 0 ]
-    then
-        echo "Adding user failed!"
-        exit 1
-    fi
-}
 
 checkUserExists() {
     /usr/sbin/oo-admin-ctl-user -l $userId >/dev/null 2>&1
@@ -174,9 +105,9 @@ help() {
     echo -e "\t-enroll=<gearSize> - enroll student in one of the following courses: "
     getgearSizes
     echo -e " $bold$validGearSizes$normal"
-    echo -e "\t-disenroll         - disenroll a student from all courses"
-    echo -e "\t-batch_disenroll_by_gears   - disenroll students corresponding to a list of gear UUIDs found in a file called:  gears.txt"
-    echo -e "\t-batch_disenroll_by_accounts   - disenroll students found in a file called:  accounts.txt"
+    echo -e "\t-disenrollFromAll         - destroys all OSE apps and removes all gear sizes associated to that account(with the exception of pds_medium"
+    echo -e "\t-batch_disenroll_all_by_gears   - disenroll students corresponding to a list of gear UUIDs found in a file called:  gears.txt"
+    echo -e "\t-batch_disenroll_all_by_accounts   - disenroll students found in a file called:  accounts.txt"
     echo -e "\nEXAMPLES:"
     echo -e " Disenroll user$bold jbride-redhat.com$normal by destroying all apps and stripping all gear"
     echo -e "  sizes:"
@@ -187,6 +118,101 @@ help() {
 
     echo -e
 }
+
+determineMongoInfo() {
+
+    mongoUser=`cat /etc/openshift/broker.conf | egrep -i 'MONGO_USER='`
+    mongoUser=${mongoUser#*=}
+    mongoUser=${mongoUser//\"}
+
+    mongoPasswd=`cat /etc/openshift/broker.conf | egrep -i 'MONGO_PASSWORD='`
+    mongoPasswd=${mongoPasswd#*=}
+    mongoPasswd=${mongoPasswd//\"}
+
+    mongoDb=`cat /etc/openshift/broker.conf | egrep -i 'MONGO_DB='`
+    mongoDb=${mongoDb#*=}
+    mongoDb=${mongoDb//\"}
+    echo -en "\ndetermineMongoInfo() mongoUser=$mongoUser mongoPasswd=$mongoPasswd mongoDb=$mongoDb \n "
+}
+
+
+enroll() {
+    id $userId > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Error, user $userId does not exist in SSO.  Exiting."
+        exit 1
+    fi
+    check
+    echo -en "\nenroll():  enrolling $userId to course with gearSize = $gearSize\n"
+    /usr/sbin/oo-admin-ctl-user -c -l $userId --addgearsize $gearSize > /dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+        echo "Adding user failed!"
+        exit 1
+    fi
+}
+
+# Given a userId, destroys all OSE apps and removes all gear sizes (with the exception of pds_medium)
+disenrollFromAll() {
+    echo -en "\ndisenroll(): disenrolling $userId from all courses\n"
+    arr=`/usr/sbin/oo-admin-ctl-domain -l $userId | egrep -i '^name:'`
+    if [ -n "$arr" ]; then
+        for app in $arr;
+        do
+         if [ "$app" != "name:" ]; then
+            destroyApp $userId $app
+         fi
+       done
+    fi
+    arr=`/usr/sbin/oo-admin-ctl-user -l $userId | egrep -i '  gear sizes:'|sed -e "s/.*:\s//"|sed -e "s/,//g"`
+    if [ -n "$arr" ]; then
+        for gearSize in $arr;
+        do
+           gearSize=${gearSize//,} 
+           gearSize=${gearSize//\ } 
+           if [ "$gearSize" != "pds_medium" ]; then
+            echo "Removing gear size $gearSize for user $userId"
+            /usr/sbin/oo-admin-ctl-user -l $userId --removegearsize $gearSize >/dev/null 2>&1
+           fi
+        done
+    fi
+}
+
+batchDisenrollFromAllByAccounts() {
+    for userId in $(cat accounts.txt)
+    do
+        if [[ $userId != \#* ]] ; then
+            disenroll
+        fi
+    done
+}
+
+destroyAppGivenGearUUID() {
+
+    determineMongoInfo
+
+    eval appString=\" `mongo --username $mongoUser --password $mongoPasswd $mongoDb --eval "printjson(db.applications.find( { _id: ObjectId(\"$gearId\") }, { canonical_name: 1 } ).shellPrint() )"` \"
+    appId=`echo $appString | cut -d':' -f 5 | cut -d' ' -f 2 | cut -d',' -f 1`
+    echo $appId
+
+}
+
+batchdisenrollbygears() {
+
+    determineMongoInfo
+
+    for gearId in $(cat gears.txt)
+    do
+        if [[ $gearId != \#* ]] ; then
+            eval userString=\" `mongo --username $mongoUser --password $mongoPasswd $mongoDb --eval "printjson(db.applications.find( { _id: ObjectId(\"$gearId\") }, { members: 1 } ).shellPrint() )"` \"
+            userId=`echo $userString | cut -d':' -f 8 | cut -d' ' -f 2 | cut -d',' -f 1`
+            echo -en "\n\n*********     $gearId userId=$userId\n"
+            disenroll
+        fi
+    done
+}
+
+
 
 if [ -z "$1" ]
 then
@@ -209,13 +235,16 @@ do
             op=enroll
             ;;
         -disenroll*)
-            op=disenroll
+            op=disenrollFromAll
             ;;
-        -batch_disenroll_by_gears*)
-            op=batchdisenrollbygears
+        -disenrollFromAll*)
+            op=disenrollFromAll
             ;;
-        -batch_disenroll_by_accounts*)
-            op=batchdisenrollbyaccounts
+        -batch_disenroll_all_by_gears*)
+            op=batchDisenrollFromAllByGears
+            ;;
+        -batch_disenroll_all_by_accounts*)
+            op=batchDisenrollFromAllByAccounts
             ;;
         *)  echo "unknown command line parameter: $var .  Execute -help for details of valid parameters. "; exit 1;
     esac
--- remove_gearsize_gpsetraining.sh
+++ remove_gearsize_gpsetraining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..50}
+for i in {1..52}
 do
 oo-admin-ctl-user -l gpsetraining$i --removegearsize pds_medium
 done
